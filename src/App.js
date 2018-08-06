191e5c1795097f2514df344987a18bd0362a81fd
Patrick Hanson <patrickhanson08@gmail.com>
2018-08-06 10:49:11 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -19,42 +19,37 @@ const CardGroup = (props) => (
 );
 
 const Card = (props) => (
-    <div>TODO: Implement Card Component</div>
+    <div className='cardGroup__card'>
+        <div className='card__description'>
+            <div className={`icon fa ${props.descriptionIcon}`}></div>
+            <div className='card__descriptionText'>{props.descriptionText}
+                {props.descriptionHint}</div>
+        </div>
+        <div className='card__price'>{props.price}</div>
+    </div>
 );
 
 const App = () => (
-    <div className="cardGroup">
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-thumbs-o-up card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Trial
-                </div>
-            </div>
-            <div className="card__price">Free!</div>
-        </div>
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-trophy card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Basic tier
-                    <br/>
-                    (most popular)
-                </div>
-            </div>
-            <div className="card__price">$10.00</div>
-        </div>
-        <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-bolt card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Advanced tier
-                    <br/>
-                    (only for enterprise-level professionals)
-                </div>
-            </div>
-            <div className="card__price">$6,000.00</div>
-        </div>
+    <div>
+        <CardGroup className='cardGroup'>
+            <Card
+                descriptionText='Trial'
+                descriptionIcon='fa-thumbs-o-up'
+                price='Free!' />
+
+            <Card
+                descriptionText='Basic tier'
+                descriptionIcon='fa-trophy'
+                descriptionHint='(most popular)'
+                price='$10.00' />
+
+            <Card
+                descriptionText='Advanced tier'
+                descriptionIcon='fa-bolt'
+                descriptionHint='(only for enterprise-level professionals)'
+                price='$6,000.00' />
+
+        </CardGroup>
     </div>
 );
 
